e5cd13cd95267e4fd9884ce7b26fc12b48ebce3b
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-02-18 10:58:35 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,4 @@
-$(function(){
-  last_message_id = $('.message-list__chat-box:last').data("message-id");
-  console.log(last_message_id);
+$(function(){  
      function buildHTML(message){
       if ( message.image ) {
         var html = 
@@ -43,29 +41,56 @@ $(function(){
         return html;
       };
     }
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action');
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.message-list').append(html);
-      $('form')[0].reset();
-      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.message-list').append(html);
+        $('form')[0].reset();
+        $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+      })
+      .always(function() {
+        $(".contents__button").prop("disabled", false);
+      });
     })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    })
-    .always(function() {
-      $(".contents__button").prop("disabled", false);
-    });
-})
+
+      function reloadMessages() {
+        last_message_id = $('.message-list__chat-box:last').data("message-id");
+        $.ajax({
+          url: "api/messages",
+          type: 'get',
+          dataType: 'json',
+          data: {id: last_message_id}
+        })
+        .done(function(messages) {
+          if (messages.length !== 0) {
+            var insertHTML = '';
+            $.each(messages, function(i, message) {
+              insertHTML += buildHTML(message)
+            });
+            $('.message-list').append(insertHTML);
+            $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+          }
+        })
+        .fail(function() {
+          alert('自動更新に失敗しました')
+        });
+      };
+
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
 });
